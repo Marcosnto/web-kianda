12a0e1925faf4b2b20f217a35a764e82c689ee37
Marcos Neto <marcos_nto@hotmail.com>
2024-08-10 14:59:30 -0300

    style: add Rasputin font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,21 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      k_header_bg: "#402D12",
-      k_green_dark: "#283618",
-      k_brown_light: "#DCA05E",
-      k_orange_dark: "#C98B31",
-      k_brown_dark: "#BB6C25",
-      k_yellow_dark: "#FFB703",
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        raspudin: ["Rasputin", "sans-serif"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        k_header_bg: "#402D12",
+        k_green_dark: "#283618",
+        k_brown_light: "#DCA05E",
+        k_orange_dark: "#C98B31",
+        k_brown_dark: "#BB6C25",
+        k_yellow_dark: "#FFB703",
       },
     },
   },
