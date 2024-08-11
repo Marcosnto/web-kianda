9ce07954121fcaa1dde514c2a9ff1b722a18f5e7
Marcos Neto <marcos_nto@hotmail.com>
2024-08-10 23:07:02 -0300

    build: add schadc ui and update tailwind variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,92 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       fontFamily: {
-        raspudin: ["Rasputin", "sans-serif"],
+        rasputin: ["Rasputin", "sans-serif"],
       },
       colors: {
         transparent: "transparent",
         current: "currentColor",
-        k_header_bg: "#402D12",
-        k_green_dark: "#283618",
-        k_brown_light: "#DCA05E",
-        k_orange_dark: "#C98B31",
-        k_brown_dark: "#BB6C25",
-        k_yellow_dark: "#FFB703",
+        k_brown: "rgb(var(--color-brown-kianda))", // 402D12
+        k_beige: "rgb(var(--color-beige-kianda))", // E6D7C0
+        k_green_dark: "rgb(var(--color-green-olive-dark-kianda))", // 283618
+        k_amber: "rgb(var(--color-amber-kianda))", // DCA05E
+        k_bronze: "rgb(var(--color-bronze-kianda))", // C98B31
+        k_cinnamon: "rgb(var(--color-cinnamon-kianda))", // BB6C25
+        k_orange: "rgb(var(--color-orange-kianda))", // FFB703
+        k_off_white: "rgb(var(--color-off-white-kianda))", // F3F1E4
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
