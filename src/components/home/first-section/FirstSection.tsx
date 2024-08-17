b3ab3198cd312b6fe8e7d7c4d37730f1d07b18e3
Marcos Neto <marcos_nto@hotmail.com>
2024-08-17 14:32:58 -0300

    feat: first section responsive

--- src/components/home/first-section/FirstSection.tsx
+++ src/components/home/first-section/FirstSection.tsx
@@ -3,6 +3,9 @@ import home_1 from "@/../public/imgs/home_1.jpg";
 import { BoldTitle } from "@/components/general";
 
 export function FirstSection() {
+  const responsiveCSS =
+    "sm:justify-center sm:gap-8 sm:text-2xl lg:p-10 lg:text-3xl";
+
   return (
     <section>
       <Image
@@ -10,10 +13,12 @@ export function FirstSection() {
         src={home_1}
         style={{ width: "100%", height: "auto" }}
       />
-      <div className="bg-k_vivid_orange flex h-20 w-full items-center justify-between pl-6 pr-6 text-lg text-k_brown shadow-inner">
+      <div
+        className={`flex h-20 w-full items-center justify-between bg-k_vivid_orange pl-6 pr-6 text-lg text-k_brown shadow-inner ${responsiveCSS}`}
+      >
         <BoldTitle>Diversidade</BoldTitle>
         <span>•</span>
-        <p className="text-xl">Cuidado</p>
+        <p className="text-xl sm:text-2xl lg:text-3xl">Cuidado</p>
         <span>•</span>
         <BoldTitle>Acolhimento</BoldTitle>
       </div>
