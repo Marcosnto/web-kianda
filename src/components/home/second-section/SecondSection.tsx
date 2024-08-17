1172c5cbdcf88670fced7783c37b1176d92806cd
Marcos Neto <marcos_nto@hotmail.com>
2024-08-17 17:50:47 -0300

    refactor: second section responsive

--- src/components/home/second-section/SecondSection.tsx
+++ src/components/home/second-section/SecondSection.tsx
@@ -5,17 +5,24 @@ import { Card } from "@/components/card";
 
 export function SecondSection() {
   const { whatKiandaDoCards } = useSecondSection();
+  const responsiveBodyCSS =
+    "md:justify-evenly xl:ml-11 xl:mr-11 xl:ml-32 xl:mr-32";
 
   return (
     <section className="mb-14 flex flex-col items-center">
       <span className="text-center">
-        <ItalicTitle className="p-8 text-xl">Como a Kianda atua</ItalicTitle>
+        <ItalicTitle className="p-8 text-xl sm:text-2xl md:text-3xl">
+          Como a Kianda atua
+        </ItalicTitle>
       </span>
-      <div className="ml-2 mr-2 w-80 gap-7">
+      <div
+        className={`ml-2 mr-2 flex flex-wrap justify-center gap-4 ${responsiveBodyCSS}`}
+      >
         {whatKiandaDoCards.map((card) => (
           <Card
             key={card.id}
-            imageClassname="w-80 h-auto"
+            className="pl-3 pr-3 md:w-[30ch]"
+            imageClassname="md:max-w-none w-full"
             label={card.label}
             summary={card.summary}
             imageAlt={card.imageAlt}
