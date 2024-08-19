00c212e4ecb4214acd97128a276e56383ffb975c
Marcos Neto <marcos_nto@hotmail.com>
2024-08-19 08:42:24 -0300

    build: rename sections file names

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,5 +1,5 @@
-export { FirstSection } from "./first-section/firstSection";
-export { SecondSection } from "./second-section/secondSection";
+export { FirstSection } from "./first-section/first-section";
+export { SecondSection } from "./second-section/second-section";
 export { ThirdSection } from "./third-section/third-section";
-export { ForthSection } from "./fourth-section/fourthSection";
-export { FifthSection } from "./fifth-section/fifthSection";
+export { ForthSection } from "./fourth-section/fourth-section";
+export { FifthSection } from "./fifth-section/fifth-section";
