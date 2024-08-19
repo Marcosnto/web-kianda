88178cc5ef2133d04582f2a4dc5ef4366be02e24
Marcos Neto <marcos_nto@hotmail.com>
2024-08-19 07:42:09 -0300

    feat: add instagram feed

--- src/components/home/fifth-section/fifthSection.hook.ts
+++ src/components/home/fifth-section/fifthSection.hook.ts
@@ -1,5 +1,41 @@
 "use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type InstagramFeedType = {
+  id: string;
+  media_type: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM";
+  media_url: string;
+  permalink: string;
+  thumbnail_url: string;
+};
+
+type MetaData = {
+  data: InstagramFeedType[];
+  paging: unknown;
+};
 
 export function useFifthSection() {
-  return {};
+  const [data, setData] = useState<InstagramFeedType[]>([]);
+
+  const token = process.env.NEXT_PUBLIC_API_INSTAGRAM;
+  const fields = "media_url,media_type,thumbnail_url,permalink";
+  const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}&limit=12`;
+
+  const getInstaFeed = async () => {
+    axios
+      .get<MetaData>(url)
+      .then((response) => setData(response.data.data))
+      .catch((error) =>
+        console.log("Ocorreu um erro ao obter dados do Instagram"),
+      );
+  };
+
+  useEffect(() => {
+    getInstaFeed();
+  }, []);
+
+  return {
+    data,
+  };
 }
--- src/components/home/fifth-section/fifthSection.tsx
+++ src/components/home/fifth-section/fifthSection.tsx
@@ -3,16 +3,42 @@ import { BoldTitle } from "@/components/general";
 import { useFifthSection } from "./fifthSection.hook";
 import insta from "@/../public/imgs/insta.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export function FifthSection() {
-  const {} = useFifthSection();
+  const { data } = useFifthSection();
 
   return (
     <section className="flex flex-col items-center gap-6 bg-[#606C38] pb-20 pt-10">
       <BoldTitle className="text-center text-[2.5rem] leading-[2.5rem] text-white">
         Kianda <br /> no #insta
       </BoldTitle>
-      <Image alt="alt" src={insta} quality={100} />
+      <div className="grid grid-cols-2 gap-3 pl-4 pr-4 sm:grid-cols-4 md:gap-1 lg:grid-cols-6">
+        {data.length === 0 ? (
+          <Image alt="alt" src={insta} quality={100} />
+        ) : (
+          data.map((item, index) => (
+            <Link
+              className={`${index > 5 ? "hidden sm:inline" : "inline"}`}
+              key={item.id}
+              href={item.permalink}
+              target="_blank"
+            >
+              <div className="h-[262px] max-w-[266px]">
+                <img
+                  className="h-full w-full object-cover"
+                  alt="Instagram image"
+                  src={
+                    item.media_type === "VIDEO"
+                      ? item.thumbnail_url
+                      : item.media_url
+                  }
+                />
+              </div>
+            </Link>
+          ))
+        )}
+      </div>
     </section>
   );
 }
