3de9e689c2ad37ea9dfbe61be22aa347d3b76b8e
Marcos Neto <marcos_nto@hotmail.com>
2024-10-05 18:05:41 -0300

    refactor: :construction: get posts from api

--- src/components/home/blog-and-articles-section/fourth-section.hook.ts
+++ src/components/home/blog-and-articles-section/fourth-section.hook.ts
@@ -1,43 +1,23 @@
-import blog_1 from "@/../public/imgs/blog_1.png";
-import blog_2 from "@/../public/imgs/blog_2.png";
-import blog_3 from "@/../public/imgs/blog_3.png";
-import { CardType } from "@/components/card/card";
+import { PostType } from "@/app/posts/page";
+import { BASE_API_URL } from "@/utils/envs";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function useForthSection() {
-  const blogCards: CardType[] = [
-    {
-      id: "1",
-      label: "Donec orci lectus aliquam ut asdadsdsdsdad",
-      author: "Marcos Neto",
-      summary:
-        "Donec orci lectus aliquam utDonec orci lectus aliquam utDonec orci lectus aliquam ut",
-      imageURL: blog_1,
-      imageAlt: "Alt descricao 1",
-      createdData: new Date("10/31/2022"),
-    },
-    {
-      id: "2",
-      label: "Nunc interdum lacus sit amet falling love",
-      author: "Marcos Neto",
-      summary:
-        "Etiam feugiat lorem non metus. Phasellus dolor. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Proin sapien ipsum, porta a, auctor .",
-      imageURL: blog_2,
-      imageAlt: "Alt descricao 2",
-      createdData: new Date("10/31/2022"),
-    },
-    {
-      id: "3",
-      label: "Título do post de blog",
-      author: "Marcos Neto",
-      summary:
-        "Crie um subtítulo que resuma de forma curta e atraente o seu post do blog para que seus visitantes queiram ler…",
-      imageURL: blog_3,
-      imageAlt: "Alt descricao 3",
-      createdData: new Date("10/31/2022"),
-    },
-  ];
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const currentPath = usePathname();
+
+  useEffect(() => {
+    (async () => {
+      const posts: PostType[] = await fetch(
+        `${BASE_API_URL}/wp-json/api/v1/articles?_limit=3`,
+      ).then((data) => data.json());
+      setPosts(posts);
+    })();
+  }, []);
 
   return {
-    blogCards,
+    posts,
+    currentPath,
   };
 }
--- src/components/home/blog-and-articles-section/fourth-section.tsx
+++ src/components/home/blog-and-articles-section/fourth-section.tsx
@@ -1,9 +1,11 @@
 import { ItalicTitle } from "@/components/general";
+import blog_1 from "@/../public/imgs/blog_1.png";
 import { useForthSection } from "./fourth-section.hook";
 import { Card } from "@/components/card";
+import { usePathname } from "next/navigation";
 
 export function ForthSection() {
-  const { blogCards } = useForthSection();
+  const { posts, currentPath } = useForthSection();
 
   return (
     <section>
@@ -13,19 +15,20 @@ export function ForthSection() {
         </ItalicTitle>
       </span>
       <div className="flex flex-wrap gap-8 bg-k_beige text-k_brown md:mb-4 md:justify-around md:bg-transparent lg:justify-around xl:justify-center xl:gap-12">
-        {blogCards.map((card) => (
+        {posts.map((post) => (
           <Card
             className={`vs:w-full md:w-[unset] md:rounded md:bg-k_beige md:shadow-md lg:w-[20.375rem] xl:w-[24.375rem]`}
             bodyClassName="pl-[17.3px] pr-[17.3px] max-w-none md:max-w-[41ch]"
             imageClassName="h-72 w-full"
             summaryClassName="text-sm/relaxed"
-            key={card.id}
-            label={card.label}
-            summary={card.summary}
-            author={card.author}
-            createdData={card.createdData}
-            imageAlt={card.imageAlt}
-            imageURL={card.imageURL}
+            key={post.id}
+            label={post.title}
+            summary={post.description}
+            author={post.author}
+            createdData={new Date(post.createdData)}
+            imageAlt={post.imageDescription}
+            imageURL={blog_1}
+            url={`${currentPath + "posts" + "/" + post.id}`}
           />
         ))}
       </div>
