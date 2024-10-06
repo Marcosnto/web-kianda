8f4738ee782f0322f44c2cc63d4849e99ac3e847
Marcos Neto <marcos_nto@hotmail.com>
2024-10-06 20:11:44 -0300

    refactor: :construction_worker: create api route to instagram

--- src/components/home/instagram-section/fifth-section.hook.ts
+++ src/components/home/instagram-section/fifth-section.hook.ts
@@ -1,36 +1,20 @@
 "use client";
 import { useCallback, useEffect, useState } from "react";
-import { INSTA_API_URL } from "@/utils/envs";
 import axios from "axios";
-
-type InstagramFeedType = {
-  id: string;
-  media_type: "IMAGE" | "VIDEO" | "CAROUSEL_ALBUM";
-  media_url: string;
-  permalink: string;
-  thumbnail_url: string;
-};
-
-type MetaData = {
-  data: InstagramFeedType[];
-  paging: unknown;
-};
+import { InstagramFeedType, MetaData } from "@/app/api/instagram/route";
 
 export function useFifthSection() {
   const [data, setData] = useState<InstagramFeedType[]>([]);
 
-  const token = INSTA_API_URL;
-  const fields = "media_url,media_type,thumbnail_url,permalink";
-  const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}&limit=12`;
-
+  //TODO: add loading
   const getInstaFeed = useCallback(async () => {
     axios
-      .get<MetaData>(url)
+      .get<MetaData>("/api/instagram")
       .then((response) => setData(response.data.data))
       .catch((error) =>
         console.log("Ocorreu um erro ao obter dados do Instagram"),
       );
-  }, [url]);
+  }, []);
 
   useEffect(() => {
     getInstaFeed();
